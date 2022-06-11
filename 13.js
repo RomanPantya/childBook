abef680af0108bc18bb9d5f8060d7047504179d2
roma <romanpantya@gmail.com>
2022-06-11 23:37:26 +0300

    Two

--- 13.js
+++ 13.js
@@ -1,12 +1,10 @@
 function myPush(arr, ...params) {
-  arr.length += params.length;
 
   for (let i = 0; i < params.length; i++) {
-    arr[i + arr.length - params.length] = params[i];
+    arr[arr.length] = params[i];
   }
   return arr.length;
 };
-const a = [5, 8, "wert"];
-console.log(myPush(a, 3, 8, "rew"));
-
+const a = [ 5, 8, 'wert'];
+console.log(myPush(a, 3, 8, 'rew'));
 console.log(a);
